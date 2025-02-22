ce2e206fe63898d0883a1188b164fb45e848cea5
大城亮 <hiltoc.katsu@gmail.com>
2025-02-22 19:02:36 +0900

    エンドポイント修正・README追加

--- app/laravel-backend/routes/web.php
+++ app/laravel-backend/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/items');
 });
 
 Route::resource('items', ItemController::class);
